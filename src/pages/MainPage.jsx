819fb52698122fbfc2bf072c568421d0ddfb6362
MihaiIen <ienachevici.mihai@gmail.com>
2025-05-28 10:47:37 +0300

    butoane

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -1,20 +1,32 @@
 import React from "react";
 import "../App.css";
+import { useNavigate } from "react-router-dom";
 
 function MainPage() {
+  const navigate = useNavigate();
+
   return (
     <div className="container">
       <img src="/titlu.png" alt="Gazeta Căsătoriilor" className="titlu" />
-
       <img src="/usti.png" alt="Usti" className="usti" />
 
       <p className="mesaj">
-        Nu fiți sfioși, dragilor! Zâmbiți, distrați-vă… și promit că o să iasă și pozele bune!
+        Hei! Sunt Usturoi. Nu vă sfiiți, zâmbiți larg și încărcați cele mai haioase poze! Eu le analizez cu atenție de acasă și dau note la stil!
       </p>
 
       <div className="butoane">
-        <img src="/incarca-poza.png" alt="Încarcă o poză" className="buton" />
-        <img src="/vizualizeaza-galeria.png" alt="Vizualizează galeria" className="buton" />
+        <img
+          src="/incarca-poza.png"
+          alt="Încarcă o poză"
+          className="buton"
+          onClick={() => navigate("/upload")}
+        />
+        <img
+          src="/vizualizeaza-galeria.png"
+          alt="Vizualizează galeria"
+          className="buton"
+          onClick={() => navigate("/galerie")}
+        />
       </div>
     </div>
   );
