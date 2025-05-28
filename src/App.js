cc0013bbfc2753dfb67d088736372dbe9ba31bc7
MihaiIen <ienachevici.mihai@gmail.com>
2025-05-28 09:03:10 +0300

    main page modifications

--- src/App.js
+++ src/App.js
@@ -5,11 +5,8 @@ function App() {
   return (
     <div className="container">
       <img src="/titlu.png" alt="Gazeta Căsătoriilor" className="titlu" />
-      <img src="/usti.png" alt="Usti" className="usti" />
-      <p className="mesaj">
-        Nu fiți sfioși, dragilor! Zâmbiți, distrați-vă… și promit că o să iasă și pozele bune!<br />
-        <span className="semnatura">— Usti, paznicul veseliei 🐾</span>
-      </p>
+      <div className="linie"></div>
+      <img src="/usti.png" alt="Usti în costum" className="usti" />
       <div className="butoane">
         <img src="/incarca-poza.png" alt="Încarcă o poză" className="buton" />
         <img src="/vizualizeaza-galeria.png" alt="Vizualizează galeria" className="buton" />
