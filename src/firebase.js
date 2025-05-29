f1c045cc77c02b4e04b836ac8e0402a15a082f29
MihaiIen <ienachevici.mihai@gmail.com>
2025-05-30 01:01:36 +0300

    dada

--- src/firebase.js
+++ src/firebase.js
@@ -1,21 +1,23 @@
-// src/firebase.js
 import { initializeApp } from "firebase/app";
 import { getStorage } from "firebase/storage";
 import { getFirestore } from "firebase/firestore";
+import { initializeAppCheck, ReCaptchaV3Provider } from "firebase/app-check";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDjF6Bc_WpY8w4RguzzPa_kILGIyEpv5VQ",
+   apiKey: "AIzaSyDjF6Bc_WpY8w4RguzzPa_kILGIyEpv5VQ",
   authDomain: "event-photos-beige.firebaseapp.com",
   projectId: "event-photos-beige",
-  storageBucket: "event-photos-beige.appspot.com",
+  storageBucket: "event-photos-beige.firebasestorage.app",
   messagingSenderId: "1077793789573",
   appId: "1:1077793789573:web:864d6ecfc006af728e7bec"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
-// Export Storage and Firestore
+const appCheck = initializeAppCheck(app, {
+  provider: new ReCaptchaV3Provider("SITE_KEY"),
+  isTokenAutoRefreshEnabled: true
+});
+
 export const storage = getStorage(app);
 export const db = getFirestore(app);
-export { app };
