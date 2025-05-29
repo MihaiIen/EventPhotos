e05f25d92efb745601d7ecbad749fa4ed8d9e5f4
MihaiIen <ienachevici.mihai@gmail.com>
2025-05-29 23:50:37 +0300

    dore

--- src/App.js
+++ src/App.js
@@ -1,20 +1,16 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom"; // fără Router
 import MainPage from "./pages/MainPage";
 import MesajeSecrete from "./pages/MesajeSecrete";
 import GaleriePage from "./pages/GaleriePage";
 
-
-
 function App() {
   return (
     <Routes>
-  <Route path="/" element={<MainPage />} />
-  <Route path="/galerie" element={<GaleriePage />} />
-  <Route path="/mesaj" element={<MesajeSecrete />} />
-</Routes>
+      <Route path="/" element={<MainPage />} />
+      <Route path="/galerie" element={<GaleriePage />} />
+      <Route path="/mesaj" element={<MesajeSecrete />} />
+    </Routes>
   );
 }
 
 export default App;
-
-
