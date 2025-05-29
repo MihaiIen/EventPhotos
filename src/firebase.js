712553783b63326e3a38ddee3195e47e45a39678
MihaiIen <ienachevici.mihai@gmail.com>
2025-05-30 01:28:13 +0300

    dodo

--- src/firebase.js
+++ src/firebase.js
@@ -4,13 +4,14 @@ import { getFirestore } from "firebase/firestore";
 import { initializeAppCheck, ReCaptchaV3Provider } from "firebase/app-check";
 
 const firebaseConfig = {
-   apiKey: "AIzaSyDjF6Bc_WpY8w4RguzzPa_kILGIyEpv5VQ",
+  apiKey: "AIzaSyDjF6Bc_WpY8w4RguzzPa_kILGIyEpv5VQ",
   authDomain: "event-photos-beige.firebaseapp.com",
   projectId: "event-photos-beige",
-  storageBucket: "event-photos-beige.firebasestorage.app",
+  storageBucket: "event-photos-beige.appspot.com", // <-- aici era greșeala
   messagingSenderId: "1077793789573",
   appId: "1:1077793789573:web:864d6ecfc006af728e7bec"
 };
+
 
 const app = initializeApp(firebaseConfig);
 
